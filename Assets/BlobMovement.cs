9c5016a951c2100ae821e5e5fcd09ad656c9c3d0
KSabah <sabahk32@hotmail.com>
2020-09-29 16:20:12 +0100

    Test sprite moving withing bounds of scene

---
+++ Assets/BlobMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D body;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        body.velocity = new Vector2(h * speed, v * speed);
+
+    }
+    
+   
+}
