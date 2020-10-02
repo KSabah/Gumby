5dced570d02e3d7cc4386fd36c73dff1048c6bdc
KSabah <sabahk32@hotmail.com>
2020-10-02 17:02:03 +0100

    Updated animation for moving both left and right, added comments

--- Assets/BlobMovement.cs
+++ Assets/BlobMovement.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlobMovement : MonoBehaviour
-{
-    public float speed;
+{  
+    private float movementSpeed = 2f;   //Set speed for blob, don't want this to be variable!
+    private bool goingRight = true;     //Default animation is to the right, so assume that's where we're going!
     public Rigidbody2D body;
 
     public Animator animator;
@@ -16,14 +17,44 @@ public class BlobMovement : MonoBehaviour
 
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        //GetAxisRaw will return value in range -1 to 1 depending on input
 
-        body.velocity = new Vector2(h * speed, v * speed);
+        //For keyboard, it's mapped to 1 for right/d and -1 for left/a. 0 is default (no press)
+        //Same applies to vertical for up/w and down/s
+        float horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        float verticalMove = Input.GetAxisRaw("Vertical") * movementSpeed;
 
-        animator.SetFloat("Speed", (h));
+        body.velocity = new Vector2(horizontalMove, verticalMove);    
+
+        //If we move blob to the right but we aren't facing right
+        if (horizontalMove > 0 && !goingRight)
+        {
+            //flip blob!
+            Flip();
+        }
+        //Otherwise if we move blob to the left but we aren't facing left
+        else if (horizontalMove < 0 && goingRight)
+        {
+            //flip blob!
+            Flip();
+        }
+        
+        animator.SetFloat("isMoving", Mathf.Abs(horizontalMove));    //If horizontalMovement > 0, then we know we are moving so apply animation
 
     }
-    
-   
+
+    private void Flip()
+    {
+        //Switch the way the player is labelled as facing.
+        goingRight = !goingRight;
+
+        //Get current state of the sprite and save it
+        Vector3 currentScale = transform.localScale;
+        //Multiple by *1 to flip it 
+        currentScale.x *= -1;
+        //Make this value the current state of the sprite
+        transform.localScale = currentScale;
+    }
+
+
 }
